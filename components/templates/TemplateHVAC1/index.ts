2c1c39baa2a0e36a017778e73542cb33e2856c18
Nicholas Sanford <42276774-nicholassanfor4@users.noreply.replit.com>
2025-05-07 14:00:06 +0000

    df

--- components/templates/TemplateHVAC1/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Layout } from "@/components/shared/Layout";
-export { default as Hero } from "./Hero";
-export { default as About } from "./About";
-// add Services etc. when they exist
