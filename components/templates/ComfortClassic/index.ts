b625f1d9f906744495ab2c40f8b297f3abe79bb7
Nicholas Sanford <42276774-nicholassanfor4@users.noreply.replit.com>
2025-05-20 00:48:43 +0000

    D

--- components/templates/ComfortClassic/index.ts
+++
@@ -1,9 +0,0 @@
-import Layout from './Layout';
-import Header from './Header';
-import Hero from './Hero';
-import About from './About';
-import Services from './Services';
-import Footer from './Footer';
-
-export { Header, Hero, About, Services, Footer };
-export default Layout;
