65b246d03cff91097ffeea66e10357a09e986697
n n <43781285-judymatheos@users.noreply.replit.com>
2025-06-15 17:38:51 +0000

    D

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,17 @@
 import '../styles/tokens.css'
 import type { AppProps } from 'next/app'
-import { SessionProvider } from 'next-auth/react'
 import { Providers } from '@/components/ui/providers'
 import { ErrorBoundary } from '@/components/ErrorBoundary'
 import { Analytics } from '@vercel/analytics/next'
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <ErrorBoundary>
-        <Providers>
-          <Component {...pageProps} />
-          <Analytics />
-        </Providers>
-      </ErrorBoundary>
-    </SessionProvider>
+    <ErrorBoundary>
+      <Providers>
+        <Component {...pageProps} />
+        <Analytics />
+      </Providers>
+    </ErrorBoundary>
   )
 }
 
